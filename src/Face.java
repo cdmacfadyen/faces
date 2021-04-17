17f38eff102a0786734b28e80fa79f52ebc6006d
Craig <cdmacfdayen@gmail.com>
2021-04-17 17:32:52 +0100

    Interpolating between 3 faces

--- src/Face.java
+++ src/Face.java
@@ -27,6 +27,37 @@ public class Face {
             System.out.println("Size of faces and weights does not match!");
             System.exit(1);
         }
+
+        triangles = new ArrayList<>();
+        this.parent = parent;
+
+        for(Triple indices : average.mesh) {
+            PVector p1 = average.pointOffsets.get(indices.first).copy(); // starting point
+            PVector p2 = average.pointOffsets.get(indices.second).copy(); // starting point
+            PVector p3 = average.pointOffsets.get(indices.third).copy(); // starting point
+
+            PVector c1 = average.colourOffsets.get(indices.first).copy();
+            PVector c2 = average.colourOffsets.get(indices.second).copy();
+            PVector c3 = average.colourOffsets.get(indices.third).copy();
+
+            for(int i = 0; i < faces.size(); i++) {
+                FaceParser face = faces.get(i);
+                float faceWeight = weights.get(i);
+                
+                p1.add(getWeightedPointOffset(face, faceWeight, indices.first));
+                p2.add(getWeightedPointOffset(face, faceWeight, indices.second));
+                p3.add(getWeightedPointOffset(face, faceWeight, indices.third));
+
+                c1.add(getWeightedColourOffset(face, faceWeight, indices.first));
+                c2.add(getWeightedColourOffset(face, faceWeight, indices.second));
+                c3.add(getWeightedColourOffset(face, faceWeight, indices.third));
+            }
+
+            Triangle triangle = new Triangle(p1, p2, p3, c1, c2, c3);
+            triangles.add(triangle);
+        }
+
+        Collections.sort(triangles);    // make them in the order we'll draw them. 
     }
 
     public Face(Render parent, AverageFace average) {  // all we want to do is see the average
@@ -45,9 +76,27 @@ public class Face {
             triangles.add(triangle);
         }
 
-        // Collections.sort(triangles,Collections.reverseOrder());
-        Collections.sort(triangles);
+        Collections.sort(triangles);    // make them in the order we'll draw them. 
     }
+
+    PVector getWeightedPointOffset(FaceParser face, float faceWeight, int index) {
+        PVector offset = new PVector();
+        offset = face.pointOffsets.get(index).copy()
+            .mult(face.pointOffsetWeight)
+            .mult(faceWeight);
+        
+            return offset;
+    }
+
+    PVector getWeightedColourOffset(FaceParser face, float faceWeight, int index) {
+        PVector offset = new PVector();
+        offset = face.colourOffsets.get(index).copy()
+            .mult(face.colourOffsetWeight)
+            .mult(faceWeight);
+        return offset;
+    }
+
+
 
     public void draw() {
         for(int i = 0; i < triangles.size(); i++) {
--- src/Render.java
+++ src/Render.java
@@ -10,7 +10,16 @@ public class Render extends PApplet {
     AverageFace averageFace;
     Face testFace;
 
+    Face testAverageFace;
+    Face testSingleFace;
+
     int i;
+
+    boolean drawingAverage;
+
+    ArrayList<FaceParser> facesToInterpolate;
+    ArrayList<Float> faceWeights;
+    Face faceToDraw;
 
     public static void main(String[] args) {
 
@@ -28,36 +37,80 @@ public class Render extends PApplet {
     @Override
     public void settings() {
         size(1200, 800);
-        
     }
 
     @Override
     public void setup() {
         noLoop();
-        faceParserTest = new FaceParser();
-        faceParserTest.loadFace(faceDataDir, "001");
+        drawingAverage = false;
+        // basic
         averageFace = new AverageFace();
         averageFace.loadFace(faceDataDir);
+        testAverageFace = new Face(this, averageFace);
+
+        testAverageFace.scale(0.003f);
+        testAverageFace.translate(new PVector(width/2, height/2));
+        // one face
+        averageFace = new AverageFace();
+        averageFace.loadFace(faceDataDir);
+
+        faceParserTest = new FaceParser();
+        faceParserTest.loadFace(faceDataDir, "001");
+
         ArrayList<Float> weights = new ArrayList<>();
-        // ArrayList<FaceParser> faces;
+        ArrayList<FaceParser> faces = new ArrayList<>();
+
+        faces.add(faceParserTest);
         weights.add(1f);
-        
-        testFace = new Face(this, averageFace);
 
+        testFace = new Face(this, averageFace, faces, weights);
 
+        testFace.scale(0.003f);
+        testFace.translate(new PVector(width/2, height/2));
+
+        // three faces
+        facesToInterpolate = new ArrayList<>();
+        faceWeights = new ArrayList<>();
+
+        FaceParser first = new FaceParser();
+        first.loadFace(faceDataDir, "001");
+        facesToInterpolate.add(first);
+
+        FaceParser second = new FaceParser();
+        second.loadFace(faceDataDir, "002");
+        facesToInterpolate.add(second);
+
+        FaceParser third = new FaceParser();
+        third.loadFace(faceDataDir, "003");
+        facesToInterpolate.add(third);
+
+        // all evenly weighted for now
+        faceWeights.add(0.333f);
+        faceWeights.add(0.333f);
+        faceWeights.add(0.333f);
     }
 
     @Override
     public void draw() {
         background(255);
-        
-        testFace.scale(0.003f);
-        testFace.translate(new PVector(width/2, height/2));
-        testFace.draw();
+
+        if (drawingAverage) {
+            testAverageFace.draw();
+            System.out.println("Average");
+        } else {
+            // testFace.draw();
+            Face interpolated = new Face(this, averageFace, facesToInterpolate, faceWeights);
+            interpolated.scale(0.003f);
+            interpolated.translate(new PVector(width/2, height/2));
+            interpolated.draw();
+            System.out.println("Interpolated");
+        }
+        System.out.println("Rendered.");
     }
 
     @Override
     public void mouseClicked() {
+        drawingAverage = !drawingAverage;
         redraw();
     }
 
